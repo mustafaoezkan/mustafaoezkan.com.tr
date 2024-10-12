8db5fcffa435f8f8f55630c3de41516c65c02883
mustafaozkan <info@mustafaozkan.dev>
2024-10-12 22:09:43 +0300

    Analytics log test

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
@@ -5,14 +6,19 @@ import ReactGA from 'react-ga4';
 const { NODE_ENV, REACT_APP_GA_TRACKING_ID } = process.env;
 
 if (NODE_ENV === 'production') {
-  ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
+  if (REACT_APP_GA_TRACKING_ID) {
+    console.log('Initializing Google Analytics with ID:', REACT_APP_GA_TRACKING_ID);
+    ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
+  } else {
+    console.error('Google Analytics Measurement ID is missing.');
+  }
 }
 
 const Analytics = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    if (NODE_ENV === 'production') {
+    if (NODE_ENV === 'production' && REACT_APP_GA_TRACKING_ID) {
       ReactGA.send({ hitType: 'pageview', page: pathname });
     }
   }, [pathname]);
