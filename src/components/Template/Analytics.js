e0d109ea4c916fefbc30d2f05bb0ae48bdadbd76
mustafaozkan <info@mustafaozkan.dev>
2024-10-12 21:46:17 +0300

    ga testing

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -1,8 +1,13 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 const { NODE_ENV, REACT_APP_GA_TRACKING_ID } = process.env;
+
+// eslint-disable-next-line no-console
+console.log('NODE_ENV', NODE_ENV);
+// eslint-disable-next-line no-undef, no-console
+console.log('REACT_APP_GA_TRACKING_ID', REACT_APP_GA_TRACKING_ID);
 
 if (NODE_ENV === 'production') {
   ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
