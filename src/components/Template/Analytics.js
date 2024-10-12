e7771c3b43f80fa92fec93b2c862a05536cce047
mustafaozkan <info@mustafaozkan.dev>
2024-10-12 22:05:36 +0300

    tracking id test

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -4,11 +4,6 @@ import ReactGA from 'react-ga4';
 
 const { NODE_ENV, REACT_APP_GA_TRACKING_ID } = process.env;
 
-// eslint-disable-next-line no-console
-console.log('NODE_ENV', NODE_ENV);
-// eslint-disable-next-line no-undef, no-console
-console.log('REACT_APP_GA_TRACKING_ID', REACT_APP_GA_TRACKING_ID);
-
 if (NODE_ENV === 'production') {
   ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
 }
