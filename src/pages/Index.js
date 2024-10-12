d89b1fab544c1d14a03a8a6a90a453eb20c8e6a3
mustafaozkan <info@mustafaozkan.dev>
2024-10-12 16:52:31 +0300

    bug fix

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import Contact from './Contact';
+import { Link } from 'react-router-dom';
+import Main from '../layouts/Main';
+import EmailLink from '../components/Contact/EmailLink';
+import ContactIcons from '../components/Contact/ContactIcons';
 
 const Index = () => (
-  <Contact />
+  <Main
+    description="Contact Mustafa Ozkan via email @ info@mustafaoezkan.com.tr"
+  >
+    <article className="post" id="contact">
+      <header>
+        <div className="title">
+          <h2>
+            <Link to="/contact">Contact</Link>
+          </h2>
+        </div>
+      </header>
+      <div className="email-at">
+        <p>Feel free to get in touch. You can email me at: </p>
+        <EmailLink />
+      </div>
+      <ContactIcons />
+    </article>
+  </Main>
 );
 
 export default Index;
